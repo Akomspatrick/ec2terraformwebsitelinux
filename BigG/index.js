3869ccf946d7580af6e9d967b850d59b2e43bb3f
Akomspatrick <Akomspatrick>
2023-04-12 03:39:51 +0100

    updated with new controller

--- BigG/index.js
+++ BigG/index.js
@@ -5,8 +5,37 @@ const crypto = require('crypto');
 const app = express();
 
 
+app.use(
+  "/webhooks/optimizely",
+  bodyParser.text({ type: "*/*" }),
+  (req, res, next) => {
+    console.log("It sha entered first");
+    const WEBHOOK_SECRET = process.env.OPTIMIZELY_WEBHOOK_SECRET || "Our little secret";
+    const webhook_payload = req.body;
+      console.log(WEBHOOK_SECRET);
+      console.log(webhook_payload);
+    //   const hmac = crypto.createHmac('sha1', WEBHOOK_SECRET)
+    //   const webhookDigest = hmac.update(webhook_payload).digest('hex')
 
-app.use('/webhooks/optimizely', bodyParser.text({ type: '*/*' }), (req, res, next) => {
+    //   const computedSignature = `sha1=${webhookDigest}`
+    //   const requestSignature = req.header('X-Hub-Signature')
+
+    //   if (!crypto.timingSafeEqual(Buffer.from(computedSignature, 'utf8'), Buffer.from(requestSignature, 'utf8'))) {
+    //     console.log(`[Optimizely] Signatures did not match! Do not trust webhook request")`)
+    //     res.status(500)
+    //     return
+    // }
+
+    console.log(`
+    [Optimizely] Optimizely webhook request received!
+    Signatures match! Webhook verified as coming from Optimizely
+    Download Optimizely datafile and re-instantiate the SDK Client
+    For the latest changes to take affect
+  `);
+    res.sendStatus(200);
+  }
+);
+app.use('/webhooks2/optimizely2', bodyParser.text({ type: '*/*' }), (req, res, next) => {
 
   const WEBHOOK_SECRET = process.env.OPTIMIZELY_WEBHOOK_SECRET
   const webhook_payload = req.body
@@ -35,5 +64,5 @@ app.get("/", (req, res) => {
     
 })
 app.listen(5001, () => {
-     console.log('app is running @ 3001')
+     console.log('app is running @ 5001')
  })
--- BigG/runscript.sh
+++ BigG/runscript.sh
@@ -3,4 +3,5 @@ rm -rf BigG/
 mkdir BigG
 cp -r * ./BigG
 rm -rf ./BigG/BigG
+rm -rf ./BigG/node_modules
 tar -czvf  archive.tar.gz BigG/
